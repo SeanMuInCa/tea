6a75b7aba63dfe673befc190593ab57bf2150cfe
Sean <fengying1028@hotmail.com>
2024-04-29 21:21:13 -0600

    3

--- tea/.eslintrc.cjs
+++ tea/.eslintrc.cjs
@@ -1,11 +1,15 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        'es6': true,
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "plugin:import/recommended",
+        "plugin:prettier/recommended",
+        
     ],
     "overrides": [
         {
