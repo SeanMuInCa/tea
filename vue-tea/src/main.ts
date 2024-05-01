94349d583a04975fa54b720ee4bf2c97dcf01c72
Sean <fengying1028@hotmail.com>
2024-04-30 21:24:31 -0600

    routes

--- vue-tea/src/main.ts
+++ vue-tea/src/main.ts
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import './common.css'
 const app = createApp(App)
 
 app.use(createPinia())
--- vue-tea/src/views/Cart.vue
+++ vue-tea/src/views/Cart.vue
@@ -1,6 +1,13 @@
 <template>
-  <RouterView />
+    <div>
+Cart
+    </div>
 </template>
+
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
+
 </script>
+
+<style scoped>
+
+</style>
