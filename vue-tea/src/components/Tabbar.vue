2799c1accfcfad51495352dcf6e4e8a631fd6c66
Sean <fengying1028@hotmail.com>
2024-04-30 23:50:45 -0600

    buttons

--- vue-tea/src/components/Tabbar.vue
+++ vue-tea/src/components/Tabbar.vue
@@ -1,25 +1,9 @@
 <template>
     <div class="tabbar">
         <ul>
-            <li @click="toHome">
-                <House style="width: 1rem; height: 1rem; " v-show="!isHome"/>
-                <HomeFilled style="width: 1rem; height: 1rem; " v-show="isHome"/>
-                <span>Home</span>
-            </li>
-            <li @click="toList">
-                <Document style="width: 1rem; height: 1rem; " v-show="!isList"/>
-                <List style="width: 1rem; height: 1rem; " v-show="isList"/>
-                <span>Category</span>
-            </li>
-            <li @click="toCart">
-                <ShoppingTrolley style="width: 1rem; height: 1rem; " v-show="!isCart"/>
-                <ShoppingCartFull style="width: 1rem; height: 1rem; " v-show="isCart"/>
-                <span>Cart</span>
-            </li>
-            <li @click="toProfile">
-                <User style="width: 1rem; height: 1rem; " v-show="!isProfile"/>
-                <UserFilled style="width: 1rem; height: 1rem; " v-show="isProfile"/>
-                <span>Profile</span>
+            <li v-for="(item,index) in routes" :key="index">
+                <el-button :icon="item.icon" type="" circle  size="large"></el-button>
+                <span>{{ item.title }}</span>
             </li>
         </ul>
     </div>
@@ -27,43 +11,33 @@
 
 <script setup lang="ts">
 import router from '@/router';
-import{ref} from 'vue';
-const isHome = ref(true);
-const isList = ref(false);
-const isCart = ref(false);
-const isProfile = ref(false);
-const toHome = () => {
-    
-    isHome.value = true;
-    isCart.value = false;
-    isList.value = false;
-    isProfile.value = false;
-    router.push('/');
-}
-const toList = () => {
-    
-    isHome.value = false;
-    isCart.value = false;
-    isList.value = true;
-    isProfile.value = false;
-    router.push('/list');
-}
-const toCart = () => {
-    
-    isHome.value = false;
-    isCart.value = true;
-    isList.value = false;
-    isProfile.value = false;
-    router.push('/cart');
-}
-const toProfile = () => {
-    
-    isHome.value = false;
-    isCart.value = false;
-    isList.value = false;
-    isProfile.value = true;
-    router.push('/profile');
-}
+
+const routes = [
+    {
+        title:'Home',
+        icon:'House',
+        selected:'HomeFilled',
+        path:'/'
+    },
+    {
+        title:'Category',
+        icon:'Document',
+        selected:'List',
+        path:'/list'
+    },
+    {
+        title:'Cart',
+        icon:'ShoppingTrolley',
+        selected:'ShoppingCartFull',
+        path:'/cart'
+    },
+    {
+        title:'Profile',
+        icon:'User',
+        selected:'UserFilled',
+        path:'/profile'
+    }
+]
 </script>
 <!-- <script lang="ts">
 export default {
